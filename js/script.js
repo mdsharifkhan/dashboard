f04b479ea27cfbd740c89d2f2046aa500cced203
Sharif <mohammedsharif80@gmail.com>
2020-06-12 05:03:27 +0600

    js file and many css changed

--- js/script.js
+++ js/script.js
@@ -5,4 +5,33 @@
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
+
+
+    $('.counter').counterUp({
+      delay: 10,
+      time: 2000,
+      offset: 70,
+      beginAt: 100,
+      formatter: function (n) {
+          return n.replace(/,/g, '.');
+      }
+    });
+
   });
+
+
+// =====================================
+//     script for counterup plugin
+// ======================================
+ $(document).ready(function(){
+    
+            $('.counter').counterUp({
+                delay: 10,
+                time: 1000,
+                offset: 70,
+                beginAt: 100,
+                formatter: function (n) {
+                    return n.replace(/,/g, '.');
+                }
+            });
+      });
